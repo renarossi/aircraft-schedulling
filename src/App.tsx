c144fe1ae32c0de3fc182090328915d29b4fdd49
Renato Rossi <renadlerossi+git@gmail.com>
2022-12-27 16:14:29 +0000

    feat: Add FetchAircrafts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import useAxios from 'axios-hooks';
+
 import './App.scss';
+import { Aircraft, Itinerary } from './models/models';
+
 
 function App() {
+    const tomorrow = moment().add(1, 'days').format('Do MMMM YYYY');
+    const [{ data: aircrafts, loading: loadingAircrafts, error: aircraftsError }] = useAxios(
+        'http://localhost:3001/aircrafts',
+    );
+    const [fullItinerary, setFullItinerary] = useState<Itinerary[] | undefined>([]);
+
+    useEffect(() => {
+        if (aircrafts) {
+            const fullItinerary = aircrafts.reduce((acc: Itinerary[], current: Aircraft) => [...acc, new Itinerary(current)], []);
+            setFullItinerary(fullItinerary);
+        }
+    }, [aircrafts]);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <div className="content-wrapper">
+                <div className="itinerary-header">{ tomorrow }</div>
+                <div className="panels-wrapper"></div>
+            </div>
         </div>
     );
 }
---
+++ src/models/models.ts
@@ -0,0 +1,56 @@
+export interface Flight {
+    ident: string,
+    departuretime: number,
+    arrivaltime: number,
+    readable_departure: string,
+    readable_arrival: string,
+    origin: string,
+    destination: string
+}
+
+export interface Aircraft {
+    ident: string,
+    type: string,
+    economySeats: number,
+    base: string;
+}
+
+export class Itinerary {
+    aircraft: Aircraft;
+    flights: Flight[];
+
+    constructor(aircraft: Aircraft) {
+        this.aircraft = aircraft;
+        this.flights = [];
+    }
+
+    getAircraftUsage(): string {
+        let overallUsage = 0;
+        if (this.flights.length === 0) {
+            return overallUsage.toString();
+        } else {
+            for (let i = 0; i < this.flights.length; i++) {
+                overallUsage = overallUsage + this.flights[i].arrivaltime - this.flights[i].departuretime;
+            }
+            const percentage = (100 * overallUsage) / 84000; // Consider the 40 Minutes Ground rule over 00:00
+            return percentage.toFixed(2);
+        }
+    }
+}
+
+export enum BlockType {
+    Idle = 'idle',
+    Scheduled = 'scheduled',
+    Turnaround = 'turnaround'
+}
+
+export class Block {
+    width: number;
+    type: BlockType;
+
+    constructor(width: number, type: BlockType) {
+        this.width = width;
+        this.type = type;
+    }
+}
+
